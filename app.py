ae8b2424dfd3a8255d15804b9186b7b5e771e843
Danish-Belal <danishexplore019@gmail.com>
2023-06-15 12:05:33 +0530

    🚧 Seprate Routes , Validates , swagger API

--- app.py
+++ app.py
@@ -1,96 +1,22 @@
-import uuid
-from flask import Flask , request
-from flask_smorest import abort
-from db import stores , items
+
+from flask import Flask 
+from flask_smorest import Api
+
+from resources.store import blp as StoreBlueprint
+from resources.item import blp as ItemBlueprint
 
 
 app = Flask(__name__)
 
-
-@app.get("/store")
-def get_stores():
-    return {"Stores" : list(stores.values())}
-
-@app.post("/store")
-def create_store():
-    store_data = request.get_json()
-    if "Name" not in store_data:
-        abort(400, 
-              massage = "Name is not Present")
-    store_id = uuid.uuid4().hex
-    store = {**store_data , "id" : store_id}
-    stores[store_id] = store
-    return store, 201
-
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
-    try:
-        return stores[store_id]
-    except KeyError:
-        abort(404, massage = "Store not found")
-
-@app.delete("/store/<string:store_id>")
-def deleteStore(store_id):
-    try:
-        del stores[store_id]
-        return {"massage" : "Store Deleted"}
-    except KeyError:
-        return{"massage" : "Store Not Found"}
+app.config["PROPAGATE_EXCEPTIONS"] = True
+app.config["API_TITLE"] = "Stores REST API"
+app.config["API_VERSION"] = "v1"
+app.config["OPENAPI_VERSION"] = "3.0.3"
+app.config["OPENAPI_URL_PREFIX"] = "/"
+app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+api = Api(app)
 
 
-            # <-----------------Items ------------------------>
-
-@app.get("/item")
-def get_all_items():
-    return {"items" : list(items.values())}
-
-
-@app.post("/item")
-def create_item():
-    item_data = request.get_json()
-    if(
-        "Price" not in item_data
-        or "Name" not in item_data
-        or "store_id" not in item_data
-
-    ):
-        abort(400 ,
-            massage  = "Bad request. Ensure 'Price', 'Name' , 'store-Id' ")
-    
-    item_id = uuid.uuid4().hex
-    item = {**item_data , "id": item_id} 
-    items[item_id] = item
-    return item, 201
-
-
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
-    try:
-        return items[item_id]
-    except KeyError:
-        abort(404 , massage = "Item not found")
-
-
-@app.put("/item/<string:item_id>")
-def updateItem(item_id):
-    new_item = request.get_json()
-    if "Name" and "Price" not in new_item:
-        abort(404 , massage = "Ensure to provide Name and Price")
-    try:
-        item = items[item_id]
-        item |= new_item
-        return item
-    except KeyError:
-        abort(404, massage = "Item Not Found")
-
-
-@app.delete("/item/<string:item_id>")
-def deleteItem(item_id):
-    try:
-        del items[item_id]
-        return {"massage" : "Item deleted."}
-    except KeyError:
-        abort(404, massage = "Item Not Found")
-
-
-
+api.register_blueprint(ItemBlueprint)
+api.register_blueprint(StoreBlueprint)
