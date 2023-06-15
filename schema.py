61305b23075db258c0750078378f1419f62c42b2
Danish-Belal <danishexplore019@gmail.com>
2023-06-15 20:31:17 +0530

    :airplane: Modifired Schemas , Nesting

--- schema.py
+++ schema.py
@@ -1,18 +1,26 @@
 from marshmallow import Schema, fields
 
 
-class ItemSchema(Schema):
+class PlanItemSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+    
+ 
+class PlainStoreSchema(Schema):
+    id = fields.Str(dump_only=True)
+    name = fields.Str(required=True)
 
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
+class ItemSchema(PlanItemSchema):
+    store_id = fields.Int(required = True , load_only = True)
+    store = fields.Nested(PlainStoreSchema() , dump_only= True)
 
-class StoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlanItemSchema() , dump_only = True))
+
+
