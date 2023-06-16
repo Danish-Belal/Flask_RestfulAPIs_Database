200525b484c1faaa5e18d7ab72adf8ea715bdd95
Danish-Belal <danishexplore019@gmail.com>
2023-06-17 00:34:50 +0530

    🚀 Added Shcmea using Marhsmello

--- schemas.py
+++ schemas.py
@@ -1,10 +1,12 @@
 from marshmallow import Schema, fields
 
 
+
 class PlainItemSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     price = fields.Float(required=True)
+
 
 
 class PlainStoreSchema(Schema):
